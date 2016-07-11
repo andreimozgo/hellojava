137c2428972fdeea41a46238f7a2bbeb6ee092a0
andreimozgo <andrei.mozgo@gmail.com>
2016-07-11 12:51:19 +0300

    Task4 in progress

--- src/task4/Point2.java
+++ src/task4/Point2.java
@@ -1,17 +1,47 @@
 package task4;
+/*В английском тексте каждую букву заменить ее порядковым номером 
+ * в алфавите. При выводе в одной строке печатать текст с двумя 
+ * пробелами между буквами, в следующей строке внизу под каждой 
+ * буквой печатать ее номер.
+ */
 
 public class Point2 {
 
 	public static void main(String[] args) {
-		String str = "text example for exercisez"; //пример строки
+		String str = "text, example for - exercisez!"; //пример строки
+		//создаем две переменных класса StringBuilder
+		StringBuilder strNew1 = new StringBuilder(str);//1я на основе строки str
+		StringBuilder strNew2 = new StringBuilder();//2я будет второй выводимой строкой
 
-
-		for (int i=0;i<str.length();i++){
-			System.out.print(" "+str.charAt(i)+" ");
+		//перебираем все символы строки strNew1
+		for(int i=0;i<strNew1.length()-1;i++){
+			//если подряд идут две буквы - разделяем их двумя пробелами
+			if (Character.isLetter(strNew1.charAt(i))&&Character.isLetter(strNew1.charAt(i+1))){
+				strNew1.insert(i+1,"  ");
+				i++;
+			}
 		}
-		System.out.print("\n");
-		for (int i=0;i<str.length();i++){
-			System.out.printf("%3d",(int)str.charAt(i)-96);
+		//формируем вторую строку
+		for (int i=0;i<strNew1.length();i++){
+			//если в строке strNew1 идет буква, то в strNew2 вставляем ее номер в алфавите
+			if(Character.isLetter(strNew1.charAt(i))){
+				//проверка номера буквы. если меньше 10, то после номера копируем два пробела
+				if(((int)(strNew1.charAt(i))-96)<10){ 
+					strNew2=strNew2.append((int)strNew1.charAt(i)-96);					
+				}
+				else {
+					//если номер буквы больше 9, то копируем один пробел
+					strNew2=strNew2.append((int)strNew1.charAt(i)-96);
+					i++;
+				}
+			}
+			else{
+				//если в строке strNew1 не буква, то вставляем пробел
+				strNew2=strNew2.append(' ');
+			}
 		}
+		//выводим строки
+		System.out.println(strNew1);
+		System.out.println(strNew2);
 	}
 }
---
+++ src/task4/Point4.java
@@ -0,0 +1,10 @@
+package task4;
+
+public class Point4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
