e410de0df9a4d84c37921edbcbe88af3841dadad
andreimozgo <andrei.mozgo@gmail.com>
2016-07-27 12:52:19 +0300

    1

---
+++ src/taskTen/Hello.java
@@ -0,0 +1,33 @@
+package taskTen;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Hello {
+	public static void main(String[] args)  throws IOException, ClassNotFoundException {
+		String str= new String();
+
+		String path = "serial.txt";
+		File f = new File(path);
+		f.createNewFile();
+
+		try {
+			
+			fileInputStream = new FileInputStream(path);
+			} catch (FileNotFoundException e) {
+			System.out.print("File not found");
+
+
+
+
+			}
+
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path));
+		a = (A) ois.readObject();
+		System.out.println("After: " + a);
+	}
+}
+
+
