0b26ba5181b51610ae86015652f9b005162b6557
andreimozgo <andrei.mozgo@gmail.com>
2016-08-05 16:09:11 +0300

    Taxi project Internationalized

--- src/TaxiProject/Main.java
+++ src/TaxiProject/Main.java
@@ -1,7 +1,12 @@
 package TaxiProject;
+import java.text.Collator;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 /**It's a main class of my project!
  * @author Mozgo Andrei
@@ -29,40 +34,54 @@ public class Main {
 		TaxiVip mercedes = new TaxiVip("Mercedes",30000,12,150);			
 		TaxiVip bmw = new TaxiVip("BMW",30500,13,151);
 		ArrayList<Taxi> cars = new ArrayList<Taxi>();
-
+		Locale loc = new Locale("en", "EN");
+		Collator collator = Collator.getInstance(loc);
+		String language = new String();
+		Date currentDate = new Date();
+		DateFormat dfDefault = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.DEFAULT, Locale.getDefault());
+				
 		cars.add(dacia);
 		cars.add(daewoo);
 		cars.add(toyota);		
 		cars.add(honda);
 		cars.add(mercedes);
 		cars.add(bmw);
-
+		System.out.println(dfDefault.format(currentDate));
+		System.out.println("Choose language (en,ru): ");
+		language = textInput.nextLine();
+		if (collator.compare(language, "ru") == 0) {
+			loc = new Locale("ru", "RU");
+		}
+		if (collator.compare(language, "en") == 0) {
+			loc = new Locale("en", "EN");
+		}
+		ResourceBundle rb = ResourceBundle.getBundle("resources/TaxiProject", loc);
 		for(Taxi car : cars){
 			totalPrice += car.price;
 		}
-		System.out.println("Список автомобилей до сортировки");
+		System.out.println(rb.getString("carsbeforesort"));
 		for(Taxi car : cars){
 			System.out.println(car);
 		}
-		System.out.println("Стоимость автопарка: " + totalPrice);
+		System.out.println(rb.getString("carsprice") + totalPrice);
 		Collections.sort(cars, new MyComparator ());
-		System.out.println("Список автомобилей после сортировки");
+		System.out.println(rb.getString("carsaftersort"));
 		for(Taxi car : cars){
 			System.out.println(car);
 		}
-		System.out.println("Введите минимальную скорость: ");
+		System.out.println(rb.getString("minspeed"));
 		try {
 			minSpeed = textInput.nextInt();
 		}
 		catch (java.util.InputMismatchException e) {
-			System.out.println("Вы ввели недопустимые символы.");
+			System.out.println(rb.getString("texterror"));
 		}
-		System.out.println("Введите максимальную скорость: ");
+		System.out.println(rb.getString("maxspeed"));
 		try {
 			maxSpeed = textInput.nextInt();
 		}
 		catch (java.util.InputMismatchException e) {
-			System.out.println("Вы ввели недопустимые символы.");
+			System.out.println(rb.getString("texterror"));
 		}
 		textInput.close();
 		for(Taxi car : cars){
