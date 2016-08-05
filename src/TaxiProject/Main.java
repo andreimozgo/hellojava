73fb646682d53b198389921ab5be4a40fdaee855
andreimozgo <andrei.mozgo@gmail.com>
2016-08-05 16:13:23 +0300

    Taxi project Internationalized

--- src/TaxiProject/Main.java
+++ src/TaxiProject/Main.java
@@ -1,4 +1,5 @@
 package TaxiProject;
+
 import java.text.Collator;
 import java.text.DateFormat;
 import java.util.ArrayList;
@@ -8,7 +9,10 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Scanner;
-/**It's a main class of my project!
+
+/**
+ * It's a main class of my project!
+ * 
  * @author Mozgo Andrei
  */
 
@@ -27,22 +31,23 @@ public class Main {
 		int minSpeed = -1;
 		int maxSpeed = -1;
 		Scanner textInput = new Scanner(System.in);
-		TaxiEconomy dacia = new TaxiEconomy("Dacia",10000,8,100);
-		TaxiEconomy daewoo = new TaxiEconomy("Daewoo",8000,7,110);
-		TaxiRegular toyota = new TaxiRegular("Toyota",20000,10,130);		
-		TaxiRegular honda = new TaxiRegular("Honda",21000,11,135);	
-		TaxiVip mercedes = new TaxiVip("Mercedes",30000,12,150);			
-		TaxiVip bmw = new TaxiVip("BMW",30500,13,151);
+		TaxiEconomy dacia = new TaxiEconomy("Dacia", 10000, 8, 100);
+		TaxiEconomy daewoo = new TaxiEconomy("Daewoo", 8000, 7, 110);
+		TaxiRegular toyota = new TaxiRegular("Toyota", 20000, 10, 130);
+		TaxiRegular honda = new TaxiRegular("Honda", 21000, 11, 135);
+		TaxiVip mercedes = new TaxiVip("Mercedes", 30000, 12, 150);
+		TaxiVip bmw = new TaxiVip("BMW", 30500, 13, 151);
 		ArrayList<Taxi> cars = new ArrayList<Taxi>();
 		Locale loc = new Locale("en", "EN");
 		Collator collator = Collator.getInstance(loc);
 		String language = new String();
 		Date currentDate = new Date();
-		DateFormat dfDefault = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.DEFAULT, Locale.getDefault());
-				
+		DateFormat dfDefault = DateFormat.getDateTimeInstance(DateFormat.FULL,
+				DateFormat.DEFAULT, Locale.getDefault());
+
 		cars.add(dacia);
 		cars.add(daewoo);
-		cars.add(toyota);		
+		cars.add(toyota);
 		cars.add(honda);
 		cars.add(mercedes);
 		cars.add(bmw);
@@ -55,37 +60,36 @@ public class Main {
 		if (collator.compare(language, "en") == 0) {
 			loc = new Locale("en", "EN");
 		}
-		ResourceBundle rb = ResourceBundle.getBundle("resources/TaxiProject", loc);
-		for(Taxi car : cars){
+		ResourceBundle rb = ResourceBundle.getBundle("resources/TaxiProject",
+				loc);
+		for (Taxi car : cars) {
 			totalPrice += car.price;
 		}
 		System.out.println(rb.getString("carsbeforesort"));
-		for(Taxi car : cars){
+		for (Taxi car : cars) {
 			System.out.println(car);
 		}
 		System.out.println(rb.getString("carsprice") + totalPrice);
-		Collections.sort(cars, new MyComparator ());
+		Collections.sort(cars, new MyComparator());
 		System.out.println(rb.getString("carsaftersort"));
-		for(Taxi car : cars){
+		for (Taxi car : cars) {
 			System.out.println(car);
 		}
 		System.out.println(rb.getString("minspeed"));
 		try {
 			minSpeed = textInput.nextInt();
-		}
-		catch (java.util.InputMismatchException e) {
+		} catch (java.util.InputMismatchException e) {
 			System.out.println(rb.getString("texterror"));
 		}
 		System.out.println(rb.getString("maxspeed"));
 		try {
 			maxSpeed = textInput.nextInt();
-		}
-		catch (java.util.InputMismatchException e) {
+		} catch (java.util.InputMismatchException e) {
 			System.out.println(rb.getString("texterror"));
 		}
 		textInput.close();
-		for(Taxi car : cars){
-			if(car.speed>=minSpeed&&car.speed<=maxSpeed){
+		for (Taxi car : cars) {
+			if (car.speed >= minSpeed && car.speed <= maxSpeed) {
 				System.out.println(car);
 			}
 		}
