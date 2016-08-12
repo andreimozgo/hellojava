009e5f6a0c8e8724bd66393ada5e8400e3fe94c4
andreimozgo <andrei.mozgo@gmail.com>
2016-08-12 16:27:34 +0300

    Stax formated

--- src/task19/StaxParser.java
+++ src/task19/StaxParser.java
@@ -1,54 +1,55 @@
 package task19;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-	import java.io.FileInputStream;
-	import java.io.FileNotFoundException;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
-	import javax.xml.stream.XMLInputFactory;
-	import javax.xml.stream.XMLStreamConstants;
-	import javax.xml.stream.XMLStreamException;
-	import javax.xml.stream.XMLStreamReader;
+public class StaxParser {
+	public static void main(String args[]) {
+		boolean isX = false;
+		boolean isY = false;
+		boolean isUnit = false;
+		String pointX = new String();
+		String pointY = new String();
+		XMLInputFactory factory = XMLInputFactory.newFactory();
 
-	public class StaxParser {
-		public static void main(String args[]) {
-			boolean isX = false;
-			boolean isY = false;
-			boolean isUnit = false;
-			String pointX = new String();
-			String pointY = new String();
-			XMLInputFactory factory = XMLInputFactory.newFactory();
-			
-			try {
-				XMLStreamReader reader = factory
-						.createXMLStreamReader(new FileInputStream("src/1.xml"));
-				while (reader.hasNext()) {
-					int res = reader.next();
-					if (res == XMLStreamConstants.START_ELEMENT) {
-						if (reader.getLocalName().equals("pointList"))
-							System.out.println(reader.getLocalName());
-						else if (reader.getLocalName().equals("point"))
-							System.out.print(reader.getLocalName()+ " " + reader.getAttributeValue(0) + ":");
-						else if (reader.getLocalName().equals("x"))
-							isX = true;
-						else if (reader.getLocalName().equals("y"))
-							isY = true;
-						else if (reader.getLocalName().equals("unit"))
-							isUnit = true;
-					} else if (res == XMLStreamConstants.CHARACTERS) {
-						if (isX) {
-							pointX = reader.getText();
-							isX = false;
-						} else if (isY) {
-							pointY=reader.getText();
-							isY = false;
-						} else if (isUnit) {
-							System.out.println(pointX + reader.getText() + "," + pointY + reader.getText());
-							isUnit = false;
-						}
+		try {
+			XMLStreamReader reader = factory
+					.createXMLStreamReader(new FileInputStream("src/1.xml"));
+			while (reader.hasNext()) {
+				int res = reader.next();
+				if (res == XMLStreamConstants.START_ELEMENT) {
+					if (reader.getLocalName().equals("pointList"))
+						System.out.println(reader.getLocalName());
+					else if (reader.getLocalName().equals("point"))
+						System.out.print(reader.getLocalName() + " "
+								+ reader.getAttributeValue(0) + ":");
+					else if (reader.getLocalName().equals("x"))
+						isX = true;
+					else if (reader.getLocalName().equals("y"))
+						isY = true;
+					else if (reader.getLocalName().equals("unit"))
+						isUnit = true;
+				} else if (res == XMLStreamConstants.CHARACTERS) {
+					if (isX) {
+						pointX = reader.getText();
+						isX = false;
+					} else if (isY) {
+						pointY = reader.getText();
+						isY = false;
+					} else if (isUnit) {
+						System.out.println(pointX + reader.getText() + ","
+								+ pointY + reader.getText());
+						isUnit = false;
 					}
 				}
-			} catch (FileNotFoundException | XMLStreamException e) {
-				e.printStackTrace();
 			}
+		} catch (FileNotFoundException | XMLStreamException e) {
+			e.printStackTrace();
 		}
 	}
+}
