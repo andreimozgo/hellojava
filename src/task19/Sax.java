53a8d51299075dc8b1a727a6ce613da10db4910c
andreimozgo <andrei.mozgo@gmail.com>
2016-08-14 13:35:48 +0300

    Sax parser added

--- src/task19/Sax.java
+++ src/task19/Sax.java
@@ -1,9 +1,18 @@
 package task19;
 
+import java.util.ArrayList;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class Sax extends DefaultHandler {
+	int counter = 0;
+	ArrayList<String> pointX = new ArrayList<>();
+	ArrayList<String> pointY = new ArrayList<>();
+	ArrayList<String> pointUnit = new ArrayList<>();
+	boolean isX = false;
+	boolean isY = false;
+	boolean isUnit = false;
 
 	@Override
 	public void startDocument() {
@@ -12,12 +21,37 @@ public class Sax extends DefaultHandler {
 	@Override
 	public void startElement(String uri, String localName, String qName,
 			Attributes attrs) {
-		System.out.print(localName);
+		if (localName.equals("pointList")) {
+			System.out.println(localName);
+		}
+		if (localName.equals("point")) {
+			counter++;
+		}
+		if (localName.equals("x")) {
+			isX = true;
+		}
+		if (localName.equals("y")) {
+			isY = true;
+		}
+		if (localName.equals("unit")) {
+			isUnit = true;
+		}
 	}
 
 	@Override
 	public void characters(char[] ch, int start, int length) {
-		System.out.print(" " + new String(ch, start, length) + " ");
+		if (isX) {
+			pointX.add(new String(ch, start, length));
+			isX = false;
+		}
+		if (isY) {
+			pointY.add(new String(ch, start, length));
+			isY = false;
+		}
+		if (isUnit) {
+			pointUnit.add(new String(ch, start, length));
+			isUnit = false;
+		}
 	}
 
 	@Override
@@ -26,5 +60,10 @@ public class Sax extends DefaultHandler {
 
 	@Override
 	public void endDocument() {
+		for (int i = 0; i < counter; i++) {
+			System.out.println("point " + (i + 1) + ": " + pointX.get(i)
+					+ pointUnit.get(i) + ", " + pointY.get(i)
+					+ pointUnit.get(i));
+		}
 	}
 }
